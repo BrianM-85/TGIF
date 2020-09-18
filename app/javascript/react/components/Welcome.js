df2db68db940fdc1a32889c15c79c9ef0f09cfcb
B-rye <brianmanhard@gmail.com>
2020-09-18 16:12:52 -0400

    Add tutorial image and styling to welcome message

--- app/javascript/react/components/Welcome.js
+++ app/javascript/react/components/Welcome.js
@@ -26,12 +26,13 @@ const Welcome = (props) => {
           children={handleClose => <div onClick={handleClose}>
             <img src="/images/Rewind.png" id="welcome-rewind"/>
             <h3>Welcome to the <span id="welcome-tgif-name">TGIF</span> Time Machine!</h3>
-            <p>Use this website to view historical schedule data from TGIF from 1989-1996.</p>  
+            <p>The internet's first and only source of historical schedule data from TGIF from 1989-1996.</p>  
             <p>When combined with streaming services like Hulu.com you can recreate and watch a night of television from the past!</p>
             <ol>
             <li>To view schedule informaton simply select a year and press Go.</li>
             <li>To view details about an episode tap the episode name.</li>
-            <li>If a Rerun ran on the Friday you are viewing tap the<i className="fi-refresh" /> icon to have a rerun automatically suggested for you from past seasons & episodes.</li>
+            <img src="/images/Welcome Episode Preview.png" id="welcome-episode-preview" />
+            <li>If a Rerun ran on the Friday you are viewing tap the<i className="fi-refresh" /> icon to have a rerun automatically suggested for you.</li>
             <li>Have fun!</li>
             </ol>
             <h3 id="modal-close">Close</h3></div>}
